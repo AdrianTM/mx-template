a8fb59bf5dfba081db1b5d090365c57ddd88f72a
Adrian <adrian@mxlinux.org>
2018-06-05 01:43:23 -0400

    modify build-depends

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -156,7 +156,7 @@ void MainWindow::on_buttonAbout_clicked()
         Cmd c;
         QString user = c.getOutput("logname");
         if (system("command -v mx-viewer") == 0) { // use mx-viewer if available
-            system("su " + user.toUtf8() + " -c \"mx-viewer '" + url.toUtf8() + " " + tr("License").toUtf8() + "'\"&");
+            system("su " + user.toUtf8() + " -c \"mx-viewer " + url.toUtf8() + " " + tr("License").toUtf8() + "\"&");
         } else {
             system("su " + user.toUtf8() + " -c \"xdg-open " + url.toUtf8() + "\"&");
         }
